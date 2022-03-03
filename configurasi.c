56d0b62c69533059999954b190dc53dfee9f550c
wahyu_illahi07 <64129404+Wahyuillahi123@users.noreply.github.com>
2022-03-03 18:41:21 +0700

    Update configurasi.c

--- configurasi.c
+++ configurasi.c
@@ -2,3 +2,5 @@
 // Follow my instagram : https://www.instagram.com/wahyu_illahi07/ 
 // Youtube Channel : https://www.youtube.com/playlist?list=PL2DC6n3PrEKVXN9nvzONe9idXa9BgCusj
 PL2DC6n3PrEKVXN9nvzONe9idXa9BgCusj
+
+echo 'wahyu illahi'
