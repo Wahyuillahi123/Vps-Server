34372f6529fa98cdd853c5eef56fda1f6698e108
wahyu_illahi07 <64129404+Wahyuillahi123@users.noreply.github.com>
2022-03-05 10:01:56 +0700

    Update configurasi.c

--- configurasi.c
+++ configurasi.c
@@ -1,6 +1,13 @@
 // # Support
 // Follow my instagram : https://www.instagram.com/wahyu_illahi07/ 
 // Youtube Channel : https://www.youtube.com/playlist?list=PL2DC6n3PrEKVXN9nvzONe9idXa9BgCusj
-PL2DC6n3PrEKVXN9nvzONe9idXa9BgCusj
 
-echo 'wahyu illahi'
+#include <studio.h>
+#include <stdlib.h>
+
+int main(){
+  {
+    printf("Jasakumedia.id\n");
+    return 0;
+    
+  }
